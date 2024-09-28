b9ca11cc76de50c7ee98c51d2bdef6f021c9c7cd
110adisetiawan <110adisetiawan@gmail.com>
2024-09-29 04:37:13 +0700

    Change Footer

--- index.php
+++ index.php
@@ -321,7 +321,7 @@
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Anybody Can Use This Website
+      Anybody Can Use This Website.
     </div>
   </footer><!-- End Footer -->
 
